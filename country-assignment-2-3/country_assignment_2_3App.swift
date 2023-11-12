213f2013729df56c3044230bde110bd3908006f7
evans <evan0284@algonquinlive.com>
2023-11-12 13:42:11 -0500

    Initial Commit

---
+++ country-assignment-2-3/country_assignment_2_3App.swift
@@ -0,0 +1,17 @@
+//
+//  country_assignment_2_3App.swift
+//  country-assignment-2-3
+//
+//  Created by Evans on 2023-11-12.
+//
+
+import SwiftUI
+
+@main
+struct country_assignment_2_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ country-assignment-2-3UITests/country_assignment_2_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  country_assignment_2_3UITestsLaunchTests.swift
+//  country-assignment-2-3UITests
+//
+//  Created by Evans on 2023-11-12.
+//
+
+import XCTest
+
+final class country_assignment_2_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
